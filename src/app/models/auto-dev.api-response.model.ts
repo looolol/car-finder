a6bf411f870d1745415dd732dbab4dce99d5e470
looolol <robbierankin15@gmail.com>
2024-07-07 21:02:44 -0400

    Auto-generated commit

--- src/app/models/auto-dev.api-response.model.ts
+++
@@ -1,9 +0,0 @@
-import {AutoDevApiCar} from "./auto-dev.api-car.model";
-
-export interface AutoDevApiResponse {
-  hitCounts: number,
-  promotedAggregations: any[],
-  records: AutoDevApiCar[],
-  totalCount: number,
-  totalCountFormatted: string,
-}
